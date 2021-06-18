68ef4a5caf9f3592b0fc9d2e405740164d765a95
Lance Heartclaw <lance.piatkov@gmail.com>
2021-06-18 18:33:31 +0200

    initial commit on the algorithm

---
+++ Assets/Scripts/ProgressionVector/ProgressionVector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProgressionVector
+{
+    public class ProgressionVector<A, E>
+    {
+        public IEnumerable endings;
+        public IEnumerable all_actions;
+        public IEnumerable<int> action_weights;
+
+        public ProgressionVector(IEnumerable<E> endings, IEnumerable<A> all_actions, IEnumerable<int> weights)
+        {
+            this.all_actions = all_actions;
+            this.action_weights = weights;
+            this.endings = endings;
+        }
+    }
+}
